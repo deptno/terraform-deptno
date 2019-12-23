547f54cc6a05b69ac4b1bf0a1b62e6d7eb4ba285
bglee <deptno@gmail.com>
2019-12-24 01:30:02 +0900

    remove data dynamodb

--- layers/backend.tf
+++ layers/backend.tf
@@ -1,14 +1,10 @@
-data aws_dynamodb_table state {
-	name = "deptno-tfstate"
-}
 terraform {
-	backend = "s3"
-	config = {
+	backend s3 {
 		bucket = "deptno-tfstate"
+		region = "ap-northeast-2"
 		key = "deptno.tfstate"
 		encrypt = true
 		dynamodb_table = "detpno-tfstate"
-		region = "ap-northeast-2"
 		profile = "deptno"
 	}
 }
